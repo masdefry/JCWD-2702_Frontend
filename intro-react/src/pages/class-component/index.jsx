b6283551a61f277f72390b371ad224551c47a1a6
masdefry <muhammaddefryan@gmail.com>
2024-05-30 10:52:40 +0700

    Lifecycle Method

--- intro-react/src/pages/class-component/index.jsx
+++ intro-react/src/pages/class-component/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 // State: Tempat untuk Menyimpan Data, Ketika Terjadi Perubahan Data pada State Tersebut, Component Akan di Render Ulang
-
+// Lifecycle Method: Siklus Component
+//  - ComponentDidMount     : Ketika Pertama Kali Halaman dibuka
+//  - ComponentDidUpdate    : Ketika Terjadi Perubahan Pada State
+//  - ComponentWillUnmount  : Ketika Akan Berpindah Halaman
 
 class ClassComponentPage extends React.Component{
 
@@ -13,7 +17,20 @@ class ClassComponentPage extends React.Component{
         this.setState({number: 1})
     }
 
+    componentDidMount(){
+        console.log('ComponentDidMount RUNNING!')
+    }
+
+    componentDidUpdate(){
+        console.log('ComponentDidUpdate RUNNING!')
+    }
+
+    componentWillUnmount(){
+        alert('Are you sure want to dischange your profile?')
+    }
+
     render(){
+        console.log('Render FIRST!')
         return(
             <div>
                 Class Component 
@@ -23,6 +40,9 @@ class ClassComponentPage extends React.Component{
                 <button onClick={this.onChangeNumber}>
                     Change Number 
                 </button>
+                <Link to='/admin'>
+                    Pindah Page Admin
+                </Link>
             </div>
         )
     }
