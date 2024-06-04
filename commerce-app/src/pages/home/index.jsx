c822ff18808586eb5c6eec7688290587b167e8db
masdefry <muhammaddefryan@gmail.com>
2024-06-04 14:35:53 +0700

    Fetching Products on Landing Page

--- commerce-app/src/pages/home/index.jsx
+++ commerce-app/src/pages/home/index.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
+
+// Step:
+// 01. Render Component Return
+// 02. Menjalankan Function yang Ada didalam useEffect
+// 03. Terjadi Perubahan Pada useState
+// 04. Akan Me-render Halaman Ulang
+
 export default function HomePage(){
+    const [products, setProducts] = useState(null)
+
+    const onFetch = async() => {
+        try {
+            const response = await axios.get('http://localhost:5000/products')
+            setProducts(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        onFetch()
+    }, [])
+
+    if(products === null) return <div>Loading...</div>
+
     return(
         <>
             <section name='jumbotron'>
@@ -8,84 +34,25 @@ export default function HomePage(){
                 <h1 className='text-5xl font-bold'>
                     Koleksi Terbaru 
                 </h1>
-                <div className='py-10 grid grid-cols-5 gap-10'>
-                    <div>
-                        <div>
-                            <img src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-id-master-catalog/default/dwa2830805/images/tile/5915622_1.jpg?sw=362' />
-                        </div>
-                        <h1 className='text-xl font-bold'>
-                            Sandal Wanita 
-                        </h1>
-                        <h1 className='text-xl'>
-                            Rp. 150.000, 00
-                        </h1>
-                    </div>
-                    <div>
-                        <div>
-                            <img src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-id-master-catalog/default/dwa2830805/images/tile/5915622_1.jpg?sw=362' />
-                        </div>
-                        <h1 className='text-xl font-bold'>
-                            Sandal Wanita 
-                        </h1>
-                        <h1 className='text-xl'>
-                            Rp. 150.000, 00
-                        </h1>
-                    </div>
-                    <div>
-                        <div>
-                            <img src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-id-master-catalog/default/dwa2830805/images/tile/5915622_1.jpg?sw=362' />
-                        </div>
-                        <h1 className='text-xl font-bold'>
-                            Sandal Wanita 
-                        </h1>
-                        <h1 className='text-xl'>
-                            Rp. 150.000, 00
-                        </h1>
-                    </div>
-                    <div>
-                        <div>
-                            <img src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-id-master-catalog/default/dwa2830805/images/tile/5915622_1.jpg?sw=362' />
-                        </div>
-                        <h1 className='text-xl font-bold'>
-                            Sandal Wanita 
-                        </h1>
-                        <h1 className='text-xl'>
-                            Rp. 150.000, 00
-                        </h1>
-                    </div>
-                    <div>
-                        <div>
-                            <img src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-id-master-catalog/default/dwa2830805/images/tile/5915622_1.jpg?sw=362' />
-                        </div>
-                        <h1 className='text-xl font-bold'>
-                            Sandal Wanita 
-                        </h1>
-                        <h1 className='text-xl'>
-                            Rp. 150.000, 00
-                        </h1>
-                    </div>
-                    <div>
-                        <div>
-                            <img src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-id-master-catalog/default/dwa2830805/images/tile/5915622_1.jpg?sw=362' />
-                        </div>
-                        <h1 className='text-xl font-bold'>
-                            Sandal Wanita 
-                        </h1>
-                        <h1 className='text-xl'>
-                            Rp. 150.000, 00
-                        </h1>
-                    </div>
-                    <div>
-                        <div>
-                            <img src='https://www.bata.com/dw/image/v2/BCLG_PRD/on/demandware.static/-/Sites-bata-id-master-catalog/default/dwa2830805/images/tile/5915622_1.jpg?sw=362' />
-                        </div>
-                        <h1 className='text-xl font-bold'>
-                            Sandal Wanita 
-                        </h1>
-                        <h1 className='text-xl'>
-                            Rp. 150.000, 00
-                        </h1>
-                    </div>
+                <div className='py-10 grid grid-cols-3 gap-10'>
+                    {
+                        products.map((item, index) => {
+                            return(
+                                <div>
+                                    <div>
+                                        <img src={item.imageUrl[0]} />
+                                    </div>
+                                    <h1 className='text-xl font-bold'>
+                                        {item.name}
+                                    </h1>
+                                    <h1 className='text-xl'>
+                                        Rp. {item.price.toLocaleString('id-ID')}
+                                    </h1>
+                                </div>
+                                
+                            )
+                        })
+                    }
                 </div>
             </section>
         </>
