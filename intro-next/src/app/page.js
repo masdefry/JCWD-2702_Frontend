7389f3ae603cd0696b7fc850196e10ce5e401bcf
masdefry <muhammaddefryan@gmail.com>
2024-06-07 13:20:16 +0700

    Carousel NEW

--- intro-next/src/app/page.js
+++ intro-next/src/app/page.js
@@ -2,14 +2,13 @@
 import { Carousel } from '@mantine/carousel';
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
+    <main className='relative'>
         <Carousel
-      nextControlIcon={<FaArrowRight /> }
-      previousControlIcon={<FaArrowLeft />}
+      nextControlIcon={<div className='bg-blue-300 text-white p-3 rounded-full absolute top-[-50px] right-[50px]'><FaArrowRight /></div>}
+      previousControlIcon={<div className='bg-blue-300 text-white p-3 rounded-full absolute top-[-50px] left-[50px]'><FaArrowLeft /></div>}
     >
       <Carousel.Slide>
       <div className='bg-red-100' style={{ width: '100%', height: '100px', position: 'relative' }}>
