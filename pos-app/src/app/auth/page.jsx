9b52398e75b0ca992cbd5843a7fc416b7db57a71
masdefry <muhammaddefryan@gmail.com>
2024-06-10 15:48:45 +0700

    Day12 Done

--- pos-app/src/app/auth/page.jsx
+++ pos-app/src/app/auth/page.jsx
@@ -5,19 +5,23 @@ import axios from 'axios';
 import {toast} from 'react-toastify';
 // Step-01 to Redirect Page
 import { useRouter } from 'next/navigation'
+import {useState} from 'react';
 
 export default function AuthPage(){
+    const [isLoading, setIsLoading] = useState(false)
+
     // Step-02 to Redirect Page
     const router = useRouter()
 
     const onAuth = async(_values) => {
         try{
+            setIsLoading(true)
             const res = await axios.get(`http://localhost:5000/users?username=${_values.username}`)
             if(res.data[0].password !== _values.password) throw {message: 'Account Not Found!'}
 
             toast.success('Login Success!')
             // Step-03 to Redirect Page
-            router.push('/')
+            router.push('/register')
         }catch(error){
             toast.error(error.message)
         }
@@ -36,23 +40,29 @@ export default function AuthPage(){
                         onAuth(values)
                     }}
                 >
-                    <Form>
-                        <label className='form-control w-full'>
-                            <div className='label'>
-                                <span className='label-text'>Username</span>
-                            </div>
-                            <Field name='username' type='text' className='input input-bordered w-full' />
-                        </label>   
-                        <label className='form-control w-full'>
-                            <div className='label'>
-                                <span className='label-text'>Password</span>
-                            </div>
-                            <Field name='password' type='password' className='input input-bordered w-full' />
-                        </label> 
-                        <button className='btn bg-red-500 text-white w-full mt-5'>
-                            Login
-                        </button>
-                    </Form>
+                    {
+                        ({dirty, isValid}) => {
+                            return(
+                                <Form>
+                                    <label className='form-control w-full'>
+                                        <div className='label'>
+                                            <span className='label-text'>Username</span>
+                                        </div>
+                                        <Field name='username' type='text' className='input input-bordered w-full' />
+                                    </label>   
+                                    <label className='form-control w-full'>
+                                        <div className='label'>
+                                            <span className='label-text'>Password</span>
+                                        </div>
+                                        <Field name='password' type='password' className='input input-bordered w-full' />
+                                    </label> 
+                                    <button disabled={!(dirty && isValid) || isLoading} type='submit' className='btn bg-red-500 text-white w-full mt-5'>
+                                        Login
+                                    </button>
+                                </Form>
+                            )
+                        }
+                    }
                 </Formik>
             </div>
         </section>
